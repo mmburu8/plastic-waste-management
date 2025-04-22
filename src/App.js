f9b05dd473839619f4ef14bd035b87087b3470c4
mmburu8 <mmburu961@gmail.com>
2025-04-23 00:34:41 +0300

    61

--- src/App.js
+++ src/App.js
@@ -131,7 +131,7 @@ const LineChartComponent = () => {
       labelLine={false}
       label={({name, value}) => `${name}: ${value}`}/>
       <text x={369} y={250} fontFamily="Sen" fontSize={19} textAnchor="middle">Monthly Count of</text>
-      <text x={373} y={270} fontFamily="Sen" fontSize={19} textAnchor="middle">Plastic Waste Collected</text>
+      <text x={375} y={270} fontFamily="Sen" fontSize={19} textAnchor="middle">Plastic Waste Collected</text>
 {/*{({name, value}) => `${name}: ${value}`}*/}
       <Tooltip/>
     </PieChart>
